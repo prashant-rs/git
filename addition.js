74badea04d38671df49dbed0a617283deafa9a01
prashant sonawane <prashantsonawane4047@gmail.com>
2024-02-26 20:18:30 +0530

    add arrow function

--- addition.js
+++ addition.js
@@ -3,3 +3,7 @@ function add(a, b) {
 }
 const addition = add(10, 20);
 console.log("addition is:" + addition);
+
+const addition1 = (a, b) => a + b;
+console.log(addition1(10, 20));
+console.log(addition1(100, 200));
